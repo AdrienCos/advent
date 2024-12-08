4006e21c210fee210b19e6ef7919c55e73ba0f41
Adrien Cosson <ad.cosson@gmail.com>
2024-12-08 01:43:44 +0100

    day06: optimize part 2 to run in less than 5s

--- src/day06.py
+++ src/day06.py
@@ -1,7 +1,6 @@
-from copy import deepcopy
-import enum
 from pathlib import Path
 import sys
+
 
 EXAMPLE_INPUT = """
 ....#.....
@@ -59,44 +58,82 @@ TEST_INPUT_6 = """
 ^.#.
 """
 
-# True means obstructed, False means open
-type Map = list[list[bool]]
 # Order is line, column
 type Coordinates = tuple[int, int]
-
-
-class Direction(enum.Enum):
-    UP = (-1, 0)
-    RIGHT = (0, 1)
-    DOWN = (1, 0)
-    LEFT = (0, -1)
-
-
+type Direction = tuple[int, int]
 type Step = tuple[Coordinates, Direction]
 
 
+# True means obstructed, False means open
+class Map:
+    _map: list[list[bool]]
+    _patch: Coordinates | None
+    lines: int
+    columns: int
+
+    def patch(self, coords: Coordinates) -> None:
+        self._patch = coords
+
+    def unpatch(self) -> None:
+        self._patch = None
+
+    def __init__(self, map: list[list[bool]]) -> None:
+        self._map = map
+        self._patch = None
+        self.lines = len(map)
+        self.columns = len(map[0])
+
+    def __call__(self, coords: Coordinates) -> bool | None:
+        if coords == self._patch:
+            return True
+        if (
+            coords[0] < 0
+            or coords[0] >= self.lines
+            or coords[1] < 0
+            or coords[1] >= self.columns
+        ):
+            return None
+        return self._map[coords[0]][coords[1]]
+
+    def walk(
+        self, start: Coordinates, direction: Direction
+    ) -> tuple[bool, Coordinates, list[Step]]:
+        # Return True if we left the map
+        pos = start
+        walk_steps: list[Step] = []
+        while True:
+            new_pos = move(pos, direction)
+            new_cell = self(new_pos)
+            if new_cell is None:
+                # Out of the map
+                return (True, pos, walk_steps)
+            elif new_cell:
+                # We are against an obstacle, stop there
+                return (False, pos, walk_steps)
+            else:
+                # We take another step
+                pos = new_pos
+                walk_steps.append((pos, direction))
+
+
 def turn_right(direction: Direction) -> Direction:
-    return Direction((direction.value[1], -direction.value[0]))
-
-
-def turn_back(direction: Direction) -> Direction:
-    return Direction((-direction.value[0], -direction.value[1]))
+    return (direction[1], -direction[0])
 
 
 def move(pos: Coordinates, direction: Direction) -> Coordinates:
-    return (pos[0] + direction.value[0], pos[1] + direction.value[1])
+    return (pos[0] + direction[0], pos[1] + direction[1])
 
 
 def parse_input(input: str) -> tuple[Map, Coordinates, Direction]:
     lines = input.strip().split("\n")
-    map: Map = []
+    raw_map: list[list[bool]] = []
     guard_location = (-1, -1)
     for i, line in enumerate(lines):
-        map.append([char == "#" for char in line])
+        raw_map.append([char == "#" for char in line])
         guard_column = line.find("^")
         if guard_column != -1:
             guard_location = (i, guard_column)
-    return (map, guard_location, Direction.UP)
+    return (Map(raw_map), guard_location, (-1, 0))
 
 
 def compute_path(
@@ -112,30 +149,22 @@ def compute_path(
     path: list[Step] = [(guard_pos, guard_dir)]
     guard_has_left = False
     while not guard_has_left:
-        new_pos = move(guard_pos, guard_dir)
-        # Is the new position out of bounds ?
-        if ((new_pos, guard_dir)) in visited:
+        has_left, final_pos, walk_steps = map.walk(guard_pos, guard_dir)
+        if set(walk_steps).intersection(visited):
             # The guard has entered a loop, stop here
             return None
-        if (
-            new_pos[0] < 0
-            or new_pos[0] >= len(map)
-            or new_pos[1] < 0
-            or new_pos[1] >= len(map[0])
-        ):
-            # The guard has left the map
+        if has_left:
+            # The guard has left the map, records the last steps and leave
+            visited |= set(walk_steps)
+            path += list(walk_steps)
             guard_has_left = True
             break
-        elif not map[new_pos[0]][new_pos[1]]:
-            # The cell is free, the guard moves there
-            visited.add((new_pos, guard_dir))
-            path.append((new_pos, guard_dir))
-            guard_pos = new_pos
         else:
-            # The cell is obstructed, the guard turns right
+            # The guard has not left yet, and is facing an obstacle
+            visited |= set(walk_steps)
+            path += list(walk_steps)
             guard_dir = turn_right(guard_dir)
-            visited.add((guard_pos, guard_dir))
-            # path.append((new_pos, guard_dir))
+            guard_pos = final_pos
     return path
 
 
@@ -158,23 +187,26 @@ def part2_fixed(input: str) -> int:
     # For each step of the path:
     for i, step in enumerate(clear_path[:-1]):
         # Place an obstacle on the next step
-        obstacle_coords = clear_path[i + 1]
-        if obstacle_coords[0] == guard_pos:
+        obstacle_coords = clear_path[i + 1][0]
+        if obstacle_coords == guard_pos:
             # We cannot place an obstacle on the guard's initial spot
             continue
-        if obstacle_coords[0] in [step[0] for step in clear_path[:i]]:
+        if obstacle_coords in [step[0] for step in clear_path[:i]]:
             # We cannot place an obstacle on a cell already traversed by the guard
             continue
-        # FIXME: optimize this by not copying the map by applying a mask on it
-        map_with_obstacles = deepcopy(map)
-        map_with_obstacles[obstacle_coords[0][0]][obstacle_coords[0][1]] = True
+        map.patch(obstacle_coords)
         # Try to solve the rest of the path, using the path so far
         path_or_fail = compute_path(
-            map_with_obstacles, step[0], step[1], set(clear_path[:i])
+            map,
+            step[0],
+            step[1],
+            set(clear_path[:i]),
         )
+        # Remove the patch
+        map.unpatch()
         # If the solve fails, we entered a loop, so the obstacle is useful
         if path_or_fail is None:
-            obstacles.add(obstacle_coords[0])
+            obstacles.add(obstacle_coords)
     return len(obstacles)
 
 
