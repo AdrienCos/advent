844206550062e0b9e87668d7b617b5482f0c8809
Adrien Cosson <ad.cosson@gmail.com>
2024-12-09 23:11:52 +0100

    day07: add part2 solution

--- justfile
+++ justfile
@@ -1,8 +1,11 @@
 run day:
     sops exec-file inputs/day{{ day }}.txt.enc "./venv/bin/python src/day{{ day }}.py {}"
 
-benchmark day:
+time day:
     sops exec-file --no-fifo inputs/day{{ day }}.txt.enc "hyperfine \"./venv/bin/python src/day{{ day }}.py {}\""
+
+benchmark day:
+    sops exec-file --no-fifo inputs/day{{ day }}.txt.enc "poop \"./venv/bin/python src/day{{ day }}.py {}\""
 
 show-input day:
     sops decrypt inputs/day{{ day }}.txt.enc
--- src/day07.py
+++ src/day07.py
@@ -14,7 +14,7 @@ EXAMPLE_INPUT = """
 292: 11 6 16 20
 """
 
-type Equation = tuple[int, list[int]]
+type Equation = tuple[int, tuple[int, ...]]
 
 
 def parse_input(input: str) -> list[Equation]:
@@ -23,13 +23,13 @@ def parse_input(input: str) -> list[Equation]:
     for line in lines:
         (result_raw, operands_raw) = line.split(": ")
         result = int(result_raw)
-        operands = [int(op) for op in (operands_raw.split(" "))]
+        operands = tuple([int(op) for op in (operands_raw.split(" "))])
         equations.append((result, operands))
     return equations
 
 
 def try_solve_recursive(
-    target_value: int, current_value: int, remaining_operands: list[int]
+    target_value: int, current_value: int, remaining_operands: tuple[int, ...]
 ) -> bool:
     if current_value == target_value:
         return True
@@ -46,34 +46,90 @@ def try_solve_recursive(
         )
 
 
-def is_solvable(equation: Equation) -> bool:
-    result = try_solve_recursive(equation[0], equation[1][0], equation[1][1:])
-    return result
+def try_solve_recursive_advanced_backwards(
+    current_value: int,
+    remaining_operands: tuple[int, ...],
+    # Not computing the length at each iteration saves a tiny amount of time
+    nb_remaining_operands: int,
+) -> bool:
+    if current_value == 0:
+        return True
+    elif current_value < 0:
+        return False
+    elif nb_remaining_operands == 0:
+        return False
+    else:
+        new_operand = remaining_operands[-1]
+        if current_value == new_operand:
+            return True
+
+        # Multiply case
+        if current_value % new_operand == 0:
+            multiply_result = try_solve_recursive_advanced_backwards(
+                int(current_value / new_operand),
+                remaining_operands[:-1],
+                nb_remaining_operands - 1,
+            )
+        else:
+            multiply_result = False
+
+        # Concat case
+        if str(current_value).endswith(str(new_operand)):
+            try:
+                new_current_value = int(str(current_value)[: -len(str(new_operand))])
+            except ValueError:
+                new_current_value = 0
+            concat_result = try_solve_recursive_advanced_backwards(
+                new_current_value,
+                remaining_operands[:-1],
+                nb_remaining_operands - 1,
+            )
+        else:
+            concat_result = False
+
+        # Add case
+        if current_value > new_operand:
+            add_result = try_solve_recursive_advanced_backwards(
+                current_value - new_operand,
+                remaining_operands[:-1],
+                nb_remaining_operands - 1,
+            )
+        else:
+            return False
+
+        return multiply_result or concat_result or add_result
 
 
 def part1(input: str) -> int:
     equations = parse_input(input)
     count = 0
     for equation in equations:
-        if is_solvable(equation):
+        if try_solve_recursive(equation[0], equation[1][0], equation[1][1:]):
             count += equation[0]
     return count
 
 
 def part2(input: str) -> int:
-    return 0
+    equations = parse_input(input)
+    count = 0
+    for equation in equations:
+        if try_solve_recursive_advanced_backwards(
+            equation[0], equation[1], len(equation[1])
+        ):
+            count += equation[0]
+    return count
 
 
 """
-Benchmark 1 (10 runs): ./venv/bin/python src/day09.py /tmp/.sops3896349036/tmp-file421800216
+Benchmark 1 (44 runs): ./venv/bin/python src/day07.py /tmp/.sops2502108391/tmp-file3713580327
     measurement          mean ± σ            min … max           outliers
-    wall_time           520ms ± 14.7ms     506ms …  552ms          0 ( 0%)
-    peak_rss           19.6MB ± 65.3KB    19.5MB … 19.7MB          0 ( 0%)
-    cpu_cycles         1.88G  ± 19.9M     1.82G  … 1.89G           1 (10%)
-    instructions       5.75G  ± 46.8M     5.66G  … 5.79G           1 (10%)
-    cache_references   2.86M  ± 98.6K     2.66M  … 2.98M           0 ( 0%)
-    cache_misses        588K  ±  150K      347K  …  782K           0 ( 0%)
-    branch_misses      1.16M  ± 94.0K      991K  … 1.25M           0 ( 0%)
+    wall_time           114ms ± 1.35ms     112ms …  118ms          1 ( 2%)
+    peak_rss           12.8MB ± 41.8KB    12.7MB … 12.9MB          5 (11%)
+    cpu_cycles          513M  ± 4.81M      508M  …  524M           1 ( 2%)
+    instructions       1.23G  ± 1.18M     1.22G  … 1.23G           0 ( 0%)
+    cache_references   5.31M  ± 56.0K     5.18M  … 5.52M           3 ( 7%)
+    cache_misses        325K  ± 48.8K      277K  …  600K           1 ( 2%)
+    branch_misses      2.20M  ±  125K     2.08M  … 2.56M           1 ( 2%)
 """
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -85,6 +141,6 @@ if __name__ == "__main__":
     result1 = part1(input_text)
     print(result1)
 
-    assert part2(EXAMPLE_INPUT) == 0
+    assert part2(EXAMPLE_INPUT) == 11387
     result2 = part2(input_text)
     print(result2)
